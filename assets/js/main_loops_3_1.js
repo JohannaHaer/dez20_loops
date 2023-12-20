844396ccd277933dcd88588ec76a300596fc14a4
JohannaHaer <johanna.haerdter@gmail.com>
2023-12-20 14:44:16 +0100

    3-1

---
+++ assets/js/main_loops_3_1.js
@@ -0,0 +1,14 @@
+let output = document.querySelector("#output")
+let array = []
+
+
+
+let loop = () => {
+    let input = Number(document.querySelector("#number").value)
+    
+    for (let x = 1; x <= input; x++) {
+        array.push("o")        
+    }
+    
+    output.innerHTML = `l${array.join("")}p`
+}
