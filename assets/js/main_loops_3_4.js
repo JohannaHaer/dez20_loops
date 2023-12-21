a5b70165057ccabf9873dc280db7ccbc29f4f755
JohannaHaer <johanna.haerdter@gmail.com>
2023-12-21 23:28:13 +0100

    3_4

--- assets/js/main_loops_3_4.js
+++ assets/js/main_loops_3_4.js
@@ -5,12 +5,14 @@ let count = () => {
     let firstNum = Number(document.querySelector("#firstNum").value) + 1;
     let secondNum = Number(document.querySelector("#secondNum").value) + 1;
     let sum = 0;
+    
 
     for (let index = 0; index < number; index += 1) {
-        if ((index / firstNum) % 2 === 0 || (index / secondNum) % 2 === 0) {
+        if ((index % firstNum) === 0 || (index % secondNum) === 0) {
             sum += index;
         }
+        
     }
-
+    
     return output.innerHTML = sum;
 };
